8143dab0a9e987d3a3b32f3b4c023f02cd6ac9a1
remona-minett <50187357+remona-minett@users.noreply.github.com>
2022-04-02 18:16:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
-from random import random
+import math
+import random
 from random import seed
+from datetime import datetime
 
 wt = ["sunny", "partly cloudy", "cloudy", "light snow", "snow", "heavy snow"] # weather types
 lt = ["bright", "moody", "dim", "dark", "pitchblack"] # light types
@@ -20,21 +22,42 @@ stdmonths = ["january", "february", "march", "april", "may", "june", "july", "au
 srv = ["aurora", "persephone"]
 year = [442, 643] # amount to be added to the current year, to show the correct date
 tu = "" # temperature unit - as it is evaluated to lower later, it needs to be initialized rather than None.
+sc = "" # server choice - affects what year is shown
+cd = datetime.now().day # current dmy for weather times
+cm = datetime.now().month
+cy = datetime.now().year
+temp_scale = 100
+temp_offset_x = -14
+temp_offset_y = -20
+i = 0
 
 os.system('cls||clear')
 
 print(f"Dailai Weather Forecast Generator") # version 1.0.0
 
-rs = input("Enter a seed: ") # seed the generator so you can bring up the same results if you need to regenerate it.
+rs = input("Enter a seed: ")
 if rs == "": 
     rs = random() # if you don't enter a seed, it'll create one for you.
 seed(rs) # set the seed for future use
 
-while tu.lower() not in {'f', 'c'}: # loops you until you declare what temperature variable you want
+
+
+while tu.lower() not in {'f', 'c'}:
     tu = input("Select a temperature unit, F or C: ")
 tu = tu.lower() # make it lowercase so that it doesn't need to be evaluated as to lower anymore
 
+while sc.lower() not in {'a', 'p', 'aurora', 'persephone'}: # affects what year is shown
+    sc = input("Select a server, Persephone or Aurora: ")
+if sc.lower() == 'persephone' or 'p': 
+    cy = cy + 643
+else:
+    cy = cy + 442
 
+while i < 7:    
+    temp_random = random.randint(-5, 5)
+    current_temperature = temp_scale*(math.sin((temp_offset_x+(cm*math.pi))/6)/math.pi)+temp_offset_y+temp_random # generate temperature scale
+    current_temperature = math.trunc(current_temperature)
+    print(current_temperature)
+    i += 1
 
-print(rs)
-print(f"EOF")
+print(f"{cd} {cm} {cy}")
